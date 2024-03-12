aad4f069d6100bd29984ee1876424e6e62c789cf
Ian Rodríguez <pardosito@5CG2301FYG.ds.iteso.mx>
2024-03-12 09:31:46 -0600

    Ignorando ejecutable y añadiendo archivo de prueba

--- main.py
+++ main.py
@@ -1,69 +1,24 @@
-from pandas import Series, DataFrame
 import pandas as pd
+from pandas import Series, DataFrame
 from tabulate import tabulate
 
-#Series
-ser_1 = Series([1, 1, 2, -3, -5, 8, 13])
-# print(ser_1)
-# print(ser_1.values)
-# print(ser_1.index)
+#Pedimos al usuario la cantidad de proposiciones con las que trabajará
+totalPropositions = int(input("¿Cuántas proposiciones quiere evaluar?: "))
 
-ser_2 = Series([1, 1, 2, -3, -5], index = ['a', 'b', 'c', 'd', 'e'])
-#print(ser_2)
-#print(ser_2['d'])
-#print(ser_2[ser_2 > 0])
-#print(ser_2 * 2)
+Serie1 = Series([])
+totalRows = int(2**totalPropositions)
+totalColumns = Series(Series for i in range(totalPropositions))
 
-dict_1 = {'matemáticas':80, "quimica":100, "logica":90}
-#print(dict_1)
-ser_3 = Series(dict_1)
-#print(ser_3)
+for i in totalColumns:
+    for j in range(totalRows):
+        y = " "
+        Serie1[i]._append(Series([y]))
 
-#DataFrames
+frame = {}
 
-data_1 = {
-    "Estado": ["JA","CO","MX","JA","MI"],
-    "Año": [2012, 2013, 2014, 2014, 2015],
-    "Version": [5.0, 5.1, 5.2, 4.0, 4.1]
-}
-#print(data_1)
-df_1 = DataFrame(data_1)
-#print(df_1)
+for i in totalColumns:
+    frame[i] = Serie1[i]
 
-df_2 = DataFrame(data_1, columns = ["Año", "Version", "Estado"])
-#print(df_2)
+truthTable_DF = pd.DataFrame(frame)
 
-df_3 = DataFrame(data_1, columns = ["Año", "Version", "Estado", "NewColumn"])
-#print(df_3)
-
-df_4 = df_3.rename(columns = {"NewColumn":"Rank"})
-#print(df_4)
-
-ranks_df4 = [1,1,2,3,4]
-df_4["Rank"] = ranks_df4
-
-#print(df_4["Estado"])
-#print(df_4.iloc[0])
-#print(df_4["Estado"].iloc[1])
-
-ser_nombre = Series(["maria", "lupe", "omar", "antonio", "checo"])
-ser_edad = Series([21,20,22,19,20])
-ser_estado = Series(["jalisco", "michoacan", "puebla", "guanajuato", "tabasco"])
-
-frame = {"Nombre":ser_nombre, "Edad":ser_edad, "Estado":ser_estado}
-estudiantes_df = pd.DataFrame(frame)
-#print(estudiantes_df)
-
-calificacion = [70,85,100,90,60]
-estudiantes_df["calificacion"] = calificacion
-#print(estudiantes_df)
-
-#print(estudiantes_df[["Nombre", "calificacion"]])
-
-# for index, row in estudiantes_df.iterrows():
-#     for column_name, value in estudiantes_df.iteritems():
-#         print(column_name)
-#         print(value)
-#         print()
-
-print(tabulate(estudiantes_df, headers="keys", tablefmt="psql"))
+print(tabulate(truthTable_DF, headers = "keys", tablefmt = "psql"))
