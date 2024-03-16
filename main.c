fcfcdcfd13fb12704a9f4ef8548eee130a39330a
Ian Rodríguez <pardosito@192.168.1.2>
2024-03-15 22:19:39 -0600

    Printing numbers into table and adding nested for loops for printing 0 and 1 values

--- main.c
+++ main.c
@@ -2,30 +2,30 @@
 #include <math.h>
 #include "fort.h"
 
-//void listCreator (int propositions, int rows);
+// void listCreator (int propositions, int rows);
 
 int main(void)
 {
 
-    int propositions, rows;
-    int propositionList[11] = {80,81,82,83,84,85,86,87,88,89,90}; //Chars de P a Z
-    //ft_table_t *table = ft_table_create();
-    //ft_set_cell_prop(table, 0, FT_ANY_COLUMN, FT_CPROP_ROW_TYPE, FT_ROW_HEADER);
+    int propositions, rows, i, j, k;
+    int propositionList[11] = {112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122}; // Chars de "p" a "z"
+    // ft_table_t *table = ft_create_table();
+    // ft_set_cell_prop(table, 0, FT_ANY_COLUMN, FT_CPROP_ROW_TYPE, FT_ROW_HEADER);
 
     printf("¿Cuántas proposiciones vas a evaluar?: \n");
     scanf("%d", &propositions);
-
     rows = pow(2, propositions) + 1;
 
     for (int j = 1; j <= rows; j++)
     {
         for (int i = 1; i <= propositions; i++)
         {
-            if (j == 1) printf("|%11c|",propositionList[i-1]);
-            else printf("|           |");
+            if (j == 1)
+                printf("|%-11c|", propositionList[i - 1]);
+            else
+                printf("|%-11d|", i);
         }
         printf("\n");
-        for (int z = 1; z <= propositions; z++)
         for (int z = 1; z <= propositions; z++)
         {
             printf("%.*s", 15, "-------------");
@@ -33,17 +33,17 @@ int main(void)
         printf("\n");
     }
 
-    //listCreator(propositions,rows-1);
+    //Esta lógica capaz y nos sirve para imprimir sin necesidad de la libreria
+    for (i = 0; i <= 1; i++)
+    {
+        for (j = 0; j <= 1; j++)
+        {
+            for (k = 0; k <= 1; k++)
+            {
+                printf("%d %d %d\n", i, j, k);
+            }
+        }
+    }
+
     return 0;
 }
-
-// void listCreator (int propositions, int rows){
-//     for (int i = 0; i < propositions; i++)
-//     {
-//         int x[rows];
-
-//         printf(x);
-//     }
-// }
-
-
